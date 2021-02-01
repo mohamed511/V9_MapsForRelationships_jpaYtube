338c0a981fe371a807ced0e6f0a8af2a61dc3bc6
mohamed511 <mohamedsayed194@hotmail.com>
2021-02-01 14:45:39 +0200

    main

---
+++ src/main/java/entity/Product.java
@@ -0,0 +1,50 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    private int id;
+    private String name;
+    private double price;
+    @Column(name = "expiration_date")
+    private LocalDate expirationDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+}
