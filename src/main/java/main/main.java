fb462ae2d9ea227ea23ce3eb693c3816aa6f29d9
mohamed511 <mohamedsayed194@hotmail.com>
2021-02-11 11:13:27 +0200

    first

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -1,10 +1,6 @@
 package main;
 
 
-import entity.Company;
-import entity.Detail;
-import entity.Product;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -21,12 +17,6 @@ public class main {
             // get transaction
             em.getTransaction().begin();
             System.out.println("Start...");
-            Product p = em.find(Product.class,7);
-            System.out.println(p);
-//            Product product = new Product("orange", 20);
-//            em.persist(product);
-//            Detail detail = new Detail(20, product);
-//            product.setDetail(detail);
 //            em.persist(detail);
             em.getTransaction().commit();
             em.close();
