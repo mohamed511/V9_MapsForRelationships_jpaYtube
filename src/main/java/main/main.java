a907ac4b5085f927aea94e2fab2501d8b7e03b3e
mohamed511 <mohamedsayed194@hotmail.com>
2021-02-20 09:14:43 +0200

    first

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -1,9 +1,6 @@
 package main;
 
 
-import entity.Professor;
-import entity.Student;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -21,17 +18,7 @@ public class main {
             // get transaction
             em.getTransaction().begin();
             System.out.println("Start...");
-            Student s1 = new Student("stu2",new ArrayList<>());
-            Student s2 = new Student("stu3",new ArrayList<>());
-            Professor p1 = new Professor("p2",new ArrayList<>());
-            p1.getStudents().add(s1);
-            p1.getStudents().add(s2);
 
-            s1.getProfessors().add(p1);
-            s2.getProfessors().add(p1);
-            em.persist(p1);
-            em.persist(s1);
-            em.persist(s2);
             em.getTransaction().commit();
             em.close();
         } catch (Exception e) {
